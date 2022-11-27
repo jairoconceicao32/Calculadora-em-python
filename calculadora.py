94817ecf96a517bf89cc7d224bf8c462391c4e62
nome <jairoconceicao689@gmail.com>
2022-11-27 03:34:47 -0300

    Adicionado ao entrar na calculadora a hora. Com isso foi adicionado ao projeto um script de tempo.

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,7 @@
 from funcoes.menus import  principal
+import tempo
 
-print("bem-vindo a calculadora!")
+print(f"Bem-vindo a calculadora! Agora são {tempo.hora()}")
 print("É hora de calcular!")
 
 principal.menu()
--- test.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Qual o número?"))
-resultado =1
-for j in range(1,numero +1):
-    resultado *= j
-    print(resultado)
